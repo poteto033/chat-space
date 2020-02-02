16969618b2ce1ead40d9980f499c673ec73aac1e
poteto033 <nhnhnnhn033@gmail.com>
2020-02-02 21:16:37 +0900

    new-message-get
    ・api機能を利用して最新のメッセージを取得できる様にした。

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:index,:edit, :update]
-  resources :groups
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-  end
+    namespace :api do
+      resources :messages, only: :index,defaults: { format: 'json' }
+    end
+    end
 end
